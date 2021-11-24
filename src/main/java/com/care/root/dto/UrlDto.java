db488bd1a26a4b0517a7cd260e17a563d37a308e
unknown <solly0829@gmail.com>
2021-11-24 10:29:49 +0900

    lombok.jar 설치 후 UrlDto에서 가볍게 lombok 사용해보기

--- src/main/java/com/care/root/dto/UrlDto.java
+++ src/main/java/com/care/root/dto/UrlDto.java
@@ -1,5 +1,15 @@
 package com.care.root.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UrlDto {
 
+	private String title;
+	private String url;
+	private UrlDto(String title, String url) {
+		
+	}
 }
