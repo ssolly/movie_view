01852e3153593ca10440c4315923fdb9b13549cc
unknown <solly0829@gmail.com>
2021-11-24 10:46:41 +0900

    서버단에서 URL 경로 만들어주는 기능 구현

--- src/main/java/com/care/root/dto/UrlDto.java
+++ src/main/java/com/care/root/dto/UrlDto.java
@@ -1,15 +1,18 @@
 package com.care.root.dto;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class UrlDto {
+public class UrlDto extends RepresentationModel<UrlDto> {	//상속:부모가 가지고 있는 기능을 사용가능
 
 	private String title;
 	private String url;
-	private UrlDto(String title, String url) {
-		
+	public UrlDto(String title, String url) {
+		this.title=title;
+		this.url=url;
 	}
 }
--- src/main/java/com/care/root/service/UrlService.java
+++ src/main/java/com/care/root/service/UrlService.java
@@ -1,5 +1,28 @@
 package com.care.root.service;
 
-public class UrlService {
+import java.util.ArrayList;
 
+import org.springframework.hateoas.Link;
+import org.springframework.stereotype.Service;
+
+import com.care.root.dto.UrlDto;
+
+@Service
+public class UrlService {
+	
+	//서버단에서 경로 설정
+	public ArrayList<UrlDto> getUrl() {
+		ArrayList<UrlDto> urlList = new ArrayList<UrlDto>();
+		
+		String[] url= {"/home","/searchMovie"};
+		String[] name= {"홈","영화검색"};
+		String defaultURL = "http://location:10000";
+		
+		for(int i=0; i<url.length;i++) {
+			UrlDto dto = new UrlDto(name[i], url[i]);
+			Link link = Link.of(defaultURL+url[i]);		//boot문법, 기본 경로 
+			dto.add(link.withRel(dto.getUrl()));		// getUrl : "/home", "/searchMovie"
+		}
+		return urlList;
+	}
 }
